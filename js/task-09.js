e9b192edc9adcda86bb7c2f1126d6c039575f358
AlexSanako <116287214+AlexSanako@users.noreply.github.com>
2023-03-28 16:54:11 +0300

    task-7-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const button = document.querySelector('button.change-color');
+const body = document.querySelector('body');
+const colorSpan = document.querySelector('span.color');
+
+button.addEventListener('click', () => {
+const color = getRandomHexColor();
+body.style.backgroundColor = color;
+colorSpan.textContent = color;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
